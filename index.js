e0657cdb8677fc98149c8ea37e68e13ef2079452
Edward Karogo <prezokams@gmail.com>
2025-06-22 01:45:01 +0300

    Moving Things with JavaScript by Acting on Events

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ function moveDodgerLeft() {
     }
 }
 function moveDodgerRight() {
+    //Get the current position of the dodger
     const leftNumbers = dodger.style.left.replace("px", "");
+    //Convert it to a number using parseInt()
     const left = parseInt(leftNumbers, 10);
+    //Check if its within the game field size i.e. dodger is 40px and the field is a total of 400px thus 360px remains
     if(left<360){
+        //Moves to the right 
         dodger.style.left = `${left+1}px`;
     }
 }
